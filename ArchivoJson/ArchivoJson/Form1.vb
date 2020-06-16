a9d0d5d66629ac6561400b6cb502b3aa0d0c09e4
adeleon1195 <57880266+adeleon1195@users.noreply.github.com>
2020-06-15 21:58:31 -0600

    App de escritorio , boton file y boton de carga

---
+++ ArchivoJson/ArchivoJson/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub SELECCIONAR_Click(sender As Object, e As EventArgs) Handles SELECCIONAR.Click
+
+        Dim rutaDefault = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+        Dim fileDefault = "Datos.txt"
+
+        Dim abrir As New OpenFileDialog
+        abrir.InitialDirectory = rutaDefault
+        abrir.Filter = "Archivos Datos|*txt"
+
+        If File.Exists(Path.Combine(rutaDefault, fileDefault)) Then
+            'Nombre de archivo que se cargará por defecto, si existe en la ruta
+            abrir.FileName = fileDefault
+        End If
+
+        If abrir.ShowDialog = DialogResult.OK Then
+            TextBox1.Text = abrir.FileName
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles CARGAR.Click
+
+    End Sub
+End Class
